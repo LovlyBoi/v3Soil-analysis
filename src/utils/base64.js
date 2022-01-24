e9b4fcadca017b8ebdd8ee4825663f47f6766c26
ZhiWen <728881636@qq.com>
2022-01-24 18:41:25 +0800

    feature: utils/base64-tools

---
+++ src/utils/base64.js
@@ -0,0 +1,12 @@
+function encode(str) {
+  return Buffer.from(''+str, 'utf-8').toString('base64')
+}
+
+function decode(base64Str) {
+  return Buffer.from(''+base64Str, 'base64').toString('utf-8')
+}
+
+export {
+  encode,
+  decode
+}
