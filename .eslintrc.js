96c0579135df2f0eab8be8fdd82b39626de1e75f
ZhiWen <728881636@qq.com>
2022-02-07 23:14:32 +0800

    fix: quotes、semi、eqeqeq

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,24 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:vue/essential"
-    ],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly",
-        "process": true
-    },
-    "parserOptions": {
-        "ecmaVersion": 2020,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "vue"
-    ],
-    "rules": {
-    }
-};
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: ['eslint:recommended', 'plugin:vue/essential'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+    process: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  plugins: ['vue'],
+  rules: {
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
+    eqeqeq: ['error', 'always'],
+    'no-global-assign': ['error', { exceptions: ['Object'] }],
+    'no-redeclare': 'error'
+  },
+}
--- src/network/request/config.js
+++ src/network/request/config.js
@@ -1,10 +1,10 @@
 let BASE_URL, TIME_OUT
 
-if (process.env.NODE_ENV == 'development') {
+if (process.env.NODE_ENV === 'development') {
   // 开发版本 baseURL
   BASE_URL = '/api'
   TIME_OUT = 100000
-} else if (process.env.NODE_ENV == 'production') {
+} else if (process.env.NODE_ENV === 'production') {
   // 生产版本 baseURL
   BASE_URL = 'https://semghh.xyz:10086/content1-2.0'
   TIME_OUT = 100000
